dab8a457f927aea0ead1f65ed222f579f983cb25
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-22 21:26:21 +0200

    project init

---
+++ static/lib/guiLoader.js
@@ -0,0 +1,24 @@
+window.guiLoaded = [];
+
+import { loadPack } from "/modules/uibuilder/main.js";
+
+await loadPack("/modules/material/components/pack.json", {
+  urlPrefix: "/modules/material/",
+  customStyleSheets: ["../../lib/overwrite.css"],
+});
+
+await loadPack("/lib/components/pack.json", {
+  urlPrefix: "/lib/",
+});
+
+await loadPack("/lib/input_methods/pack.json", {
+  urlPrefix: "/lib/",
+});
+
+document.body.style.opacity = 1;
+
+for (let i of guiLoaded) {
+  i();
+}
+
+window.guiLoaded = null;
