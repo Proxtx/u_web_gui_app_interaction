7448b75212a6bb7dbadc00186c59b3c9a7f45e2a
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-09-24 20:19:12 +0200

    support for single url executions

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import { listen } from "@proxtx/framework";
 import config from "@proxtx/config";
 import { setConfig } from "@proxtx/framework/static.js";
 
-setConfig({ ignoreParseHtml: ["/lib/components", "/lib/input_methods"] });
+setConfig({
+  ignoreParseHtml: ["/lib/components", "/lib/input_methods"],
+  customScriptFileExtensions: [".html", ".route"],
+});
 await listen(config.port);
 console.log("Server running. Port:", config.port);
---
+++ private/execute.js
@@ -0,0 +1,10 @@
+import { execute } from "../public/api.js";
+
+export const server = async (_document, options) => {
+  await execute(
+    options.req.cookies.pwd,
+    options.req.query.appIndex,
+    options.req.query.method,
+    JSON.parse(options.req.query.arguments)
+  );
+};
