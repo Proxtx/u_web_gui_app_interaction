8a20a96d8d9ea0c139bcd64601589c5a73ce8e16
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-11-12 17:08:53 +0100

    this hopefully helps fixing the cookie problem in iframes

--- static/lib/apiLoader.js
+++ static/lib/apiLoader.js
@@ -2,6 +2,9 @@ export const api = await framework.load("api.js");
 export const meta = await framework.load("meta.js");
 export const flow = await framework.load("flow.js");
 
+cookie.default =
+  "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/; SameSite=None; Secure";
+
 window.api = api;
 window.meta = meta;
 
