dc61f6ca9fb2bd2a3639c563e21455cb138b154a
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-02-15 19:18:50 +0100

    update offline file detection

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import config from "@proxtx/config";
 import { setConfig } from "@proxtx/framework/static.js";
 
 setConfig({
-  ignoreParseHtml: ["/lib/components", "/lib/input_methods", "/offline.html"],
+  ignoreParseHtml: ["/lib/components", "/lib/input_methods", "/offline"],
   customScriptFileExtensions: [".html", ".route"],
 });
 await listen(config.port);
