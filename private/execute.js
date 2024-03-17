e8db6fd2dca11ce9e9fe085e6fdad140b910c115
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-03-17 02:09:24 +0100

    return result of execute on execute.route

--- private/execute.js
+++ private/execute.js
@@ -1,10 +1,12 @@
 import { execute } from "../public/api.js";
 
 export const server = async (_document, options) => {
-  await execute(
-    options.req.cookies.pwd ? options.req.cookies.pwd : options.req.query.pwd,
-    options.req.query.appName,
-    options.req.query.method,
-    JSON.parse(options.req.query.arguments)
+  options.res.send(
+    await execute(
+      options.req.cookies.pwd ? options.req.cookies.pwd : options.req.query.pwd,
+      options.req.query.appName,
+      options.req.query.method,
+      JSON.parse(options.req.query.arguments)
+    )
   );
 };
